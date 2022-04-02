8363221f654e0648392512c8fd840e04436ae12b
Sh1d0w <5074917+Sh1d0w@users.noreply.github.com>
2022-04-02 11:51:19 +0300

    backup git file before replacing

--- install.sh
+++ install.sh
@@ -4,7 +4,14 @@ git --version 2>&1 >/dev/null
 GIT_IS_AVAILABLE=$?
 
 # TODO: get the path from settings
-cp ./.gitignore ~/.gitignore_global
+FILE=~/.gitignore_global
+
+if [ -f "$FILE" ]; then
+	echo "$FILE already exists. Backing up ..."
+	mv "$FILE" "${FILE}.bak_"$(date +"%Y%m%d_%H%M%S")
+fi
+
+cp ./.gitignore "$FILE"
 
 if [[ $GIT_IS_AVAILABLE -eq 0 ]]
 then
